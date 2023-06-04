0cb22eebbe4d3904176c1bc6a04d7e57e5b12478
Chris Hendrix <chendrix1123@gmail.com>
2023-06-04 13:21:44 -0400

    create signup form

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,29 @@
-const Signup = () => (
-  <main className="">
-    Signup
-  </main>
-)
+'use client'
+
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import TextInput from './textinput'
+
+const Signup: React.FC = () => {
+  const form = useForm({ mode: 'onChange' })
+
+  const onSubmit = async (data: { [x: string]: unknown }) => { console.log({ data }) }
+
+  return (
+    <div className="flex items-center justify-center">
+      <div className="px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
+        <h2 className="text-center text-2xl font-bold mb-4">Sign Up</h2>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <TextInput name="email" form={form} />
+          <TextInput name="password" form={form} />
+          <TextInput name="cpassword" form={form} />
+          <button type="submit" className="btn btn-primary w-full">
+            Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
 
 export default Signup
