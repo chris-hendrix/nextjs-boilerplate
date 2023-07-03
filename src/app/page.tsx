9046e7c1f6d739bd7996dda2a9381f1810fb08c9
Chris Hendrix <chendrix1123@gmail.com>
2023-07-03 15:33:24 -0400

    homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import Image from 'next/image'
 
 const Home = () => (
   <main className="">
-    <div>
-      <Image
-        src="/next.svg"
-        alt="Next.js Logo"
-        width={180}
-        height={37}
-        priority
-      />
+    <div className="flex flex-col items-center mt-16">
+      <h1 className="text-5xl font-bold">Hello!</h1>
+      <p className="mt-4">This is a Next.js template with user authentication using next-auth.</p>
+      <div className="flex items-center justify-center mt-12">
+        <Image
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
     </div>
   </main>
 )
