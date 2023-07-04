6f5358df6347e90a9d5311cb9b2a9a082b084138
Chris Hendrix <chendrix1123@gmail.com>
2023-07-04 14:33:22 -0400

    refactor profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,74 +1,20 @@
 'use client'
 
-import Image from 'next/image'
-import { useState } from 'react'
 import useSessionUser from '@/hooks/user'
-import { formatDate } from '@/lib/date'
 import NotFound from '@/app/not-found'
-import EditProfileModal from './edit'
+import Profile from '@/components/Profile'
 
-const Profile = () => {
+const ProfilePage = () => {
   const { user, isLoading } = useSessionUser()
-  const [modalOpen, setModalOpen] = useState(false)
-  console.log({ user, isLoading })
 
   if (!user && !isLoading) return <NotFound />
+  if (!user) return <></>
+
   return (
     <main className="px-6 py-4">
-      <div className="flex items-center justify-between bg-base-200 rounded-box p-4">
-        <div className="flex items-center">
-          <div className="relative w-12 h-12 rounded-full mr-4 overflow-hidden">
-            <Image src="/avatar.svg" alt="User Avatar" fill />
-          </div>
-          {user && (
-            <div>
-              <h3 className="text-2xl font-medium">{user?.name}</h3>
-              <p className="text-gray-500">{user?.username}</p>
-              <p className="text-gray-500">{`Joined on ${formatDate(String(user.createdAt))}`}</p>
-            </div>
-          )}
-        </div>
-        <button className="btn btn-primary" onClick={() => setModalOpen(true)}>
-          Edit Profile
-        </button>
-      </div>
-      <div className="p-4">
-      <div className="mt-8">
-        <h4 className="text-lg font-bold">Personal Info</h4>
-        <p className="text-gray-500">{user?.name}</p>
-        <p className="text-gray-500">{user?.email}</p>
-      </div>
-      <div className="mt-8">
-        <h4 className="text-lg font-bold">About</h4>
-          <p className="text-gray-500">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Sed auctor nisi eu quam tincidunt tempus.
-          </p>
-      </div>
-      <div className="mt-8">
-        <h4 className="text-lg font-bold">Linked Accounts</h4>
-        <ul className="mt-2 space-x-2">
-          <li className="inline-block">
-              <a href="#" className="text-blue-600 hover:underline">
-                Twitter
-              </a>
-          </li>
-          <li className="inline-block">
-              <a href="#" className="text-blue-600 hover:underline">
-                LinkedIn
-              </a>
-          </li>
-          <li className="inline-block">
-              <a href="#" className="text-blue-600 hover:underline">
-                GitHub
-              </a>
-          </li>
-        </ul>
-      </div>
-      </div>
-      {modalOpen && user && <EditProfileModal user={user} setOpen={setModalOpen} />}
+      <Profile user={user} canEdit />
     </main>
   )
 }
 
-export default Profile
+export default ProfilePage
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -5,30 +5,30 @@ import { useGetUsersQuery } from '@/store'
 import { formatDate } from '@/lib/date'
 import Avatar from '@/components/Avatar'
 
+const UserCard: React.FC<{ user: Partial<User> }> = ({ user }) => (
+  <div className="flex items-center justify-between bg-base-200 rounded-box p-4 mt-4 w-full">
+    <div className="flex items-center">
+      <div className="mr-4"><Avatar /></div>
+      <div>
+        <h3 className="text-2xl font-medium">{user?.name}</h3>
+        <p className="text-gray-500">{user?.username}</p>
+        <p className="text-gray-500">{`Joined on ${formatDate(String(user.createdAt))}`}</p>
+      </div>
+    </div>
+    <button className="btn btn-sm btn-primary">
+      View Profile
+    </button>
+  </div>
+)
+
 const Users: React.FC = () => {
   const { data: users } = useGetUsersQuery({ skip: 0, take: 5 })
-
-  const renderUser = (user: Partial<User>) => (
-    <div className="flex items-center justify-between bg-base-200 rounded-box p-4 mt-4">
-      <div className="flex items-center">
-        <div className="mr-4"><Avatar /></div>
-        <div>
-          <h3 className="text-2xl font-medium">{user?.name}</h3>
-          <p className="text-gray-500">{user?.username}</p>
-          <p className="text-gray-500">{`Joined on ${formatDate(String(user.createdAt))}`}</p>
-        </div>
-      </div>
-      <button className="btn btn-sm btn-primary">
-        View Profile
-      </button>
-    </div>
-  )
 
   if (!users) return <></>
 
   return (
-    <main className="px-6 py-4">
-      {users.map((user) => renderUser(user))}
+    <main className="px-6 py-4 w-full max-w-[800px]">
+      {users.map((user) => <UserCard key={user.id} user={user} />)}
   </main>
   )
 }
