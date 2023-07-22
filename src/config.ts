d3fc721d3c33ac027a5e186d9a08eb814e24c432
Chris Hendrix <chendrix1123@gmail.com>
2023-07-22 14:31:19 -0400

    Use relative api url

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const APP_URL = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : (process.env.NEXT_PUBLIC_URL || 'http://localhost:3000')
 
-export const API_URL = `${APP_URL}/api`
+export const API_URL = '/api'
 
 export const TEST_SECRET = process.env.TEST_SECRET || 'test1234'
